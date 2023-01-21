5c15b2a3bc4f4768d8eac09cb485de5a417fbe78
moslienko <8676976+moslienko@users.noreply.github.com>
2023-01-21 16:59:14 +0700

    Update  package file

--- Package.swift
+++ Package.swift
@@ -23,8 +23,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AppViewUtilits",
+            dependencies: [],
             path: "Sources"),
-            dependencies: []),
         .testTarget(
             name: "AppViewUtilitsTests",
             dependencies: ["AppViewUtilits"]),
