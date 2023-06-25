197d2c8f04968ef33be12b156115b356a146c690
moslienko <8676976+moslienko@users.noreply.github.com>
2023-06-25 16:57:20 +0700

    Add tableview example

---
+++ Example/Views/ExampleCell/ExampleCellModel.swift
@@ -0,0 +1,19 @@
+//
+//  ExampleCellModel.swift
+//  Example
+//
+//  Created by Pavel Moslienko on 12.06.2023.
+//  Copyright © 2023 moslienko. All rights reserved.
+//
+
+import AppViewUtilits
+import UIKit
+
+class ExampleCellModel: AppViewCellIdentifiable {
+    
+    let title: String
+   
+    init(title: String) {
+        self.title = title
+    }
+}
