9b83f43f467707f67e92a75bbdda6a85f2996952
moslienko <8676976+moslienko@users.noreply.github.com>
2024-09-29 10:40:34 +0700

    Add callbacks for AppTextField

--- Sources/AppViewUtilits/Components/AppTextField.swift
+++ Sources/AppViewUtilits/Components/AppTextField.swift
@@ -9,6 +9,7 @@
 #if canImport(UIKit)
 import UIKit
 
+/// Subclass `UITextField`.
 public class AppTextField: UITextField {
     
     // MARK: - Params
@@ -18,8 +19,17 @@ public class AppTextField: UITextField {
         }
     }
     
+    public var maxCharacters: Int?
+    
     // MARK: - Callbacks
-    public var didTextChanged: ((String) -> Void)?
+    public var didTextChanged: DataCallback<String>?
+    public var didBeginEditing: DataCallback<UITextField>?
+    public var didEndEditing: DataCallback<UITextField>?
+    public var shouldBeginEditing: ReturnedDataCallback<UITextField, Bool>?
+    public var shouldEndEditing: ReturnedDataCallback<UITextField, Bool>?
+    public var shouldClear: ReturnedDataCallback<UITextField, Bool>?
+    public var shouldReturn: ReturnedDataCallback<UITextField, Bool>?
+    public var shouldChangeCharactersIn: ReturnedDataCallback<(UITextField, NSRange, String), Bool>?
     
     open override func textRect(forBounds bounds: CGRect) -> CGRect {
         bounds.inset(by: textInsets)
@@ -43,8 +53,75 @@ public class AppTextField: UITextField {
         addTarget(self, action: #selector(didChangeTextFieldText), for: .editingChanged)
     }
     
+    convenience public init(
+        didTextChanged: DataCallback<String>?,
+        didBeginEditing: DataCallback<UITextField>? = nil,
+        didEndEditing: DataCallback<UITextField>? = nil,
+        shouldBeginEditing: ReturnedDataCallback<UITextField, Bool>? = nil,
+        shouldClear: ReturnedDataCallback<UITextField, Bool>? = nil,
+        shouldReturn: ReturnedDataCallback<UITextField, Bool>? = nil,
+        shouldChangeCharactersIn: ReturnedDataCallback<(UITextField, NSRange, String), Bool>? = nil
+    ) {
+        self.init(frame: .zero)
+        self.didTextChanged = didTextChanged
+        self.didBeginEditing = didBeginEditing
+        self.didEndEditing = didEndEditing
+        self.shouldBeginEditing = shouldBeginEditing
+        self.shouldClear = shouldClear
+        self.shouldReturn = shouldReturn
+        self.shouldChangeCharactersIn = shouldChangeCharactersIn
+    }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override public func awakeFromNib() {
+        super.awakeFromNib()
+        self.delegate = self
+    }
+}
+
+// MARK: - UITextFieldDelegate
+extension AppTextField: UITextFieldDelegate {
+    
+    public func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let newText = (textField.text as NSString?)?.replacingCharacters(in: range, with: string) ?? ""
+        if let maxCharacters = maxCharacters {
+            if maxCharacters > 0 && newText.count > maxCharacters {
+                return false
+            }
+            return true
+        }
+        let shouldChangeCharacters = shouldChangeCharactersIn?((self, range, string)) ?? true
+        if shouldChangeCharacters {
+            didTextChanged?(newText)
+        }
+        return shouldChangeCharacters
+    }
+    
+    public func textFieldDidBeginEditing(_ textField: UITextField) {
+        didBeginEditing?(textField)
+    }
+    
+    public func textFieldDidEndEditing(_ textField: UITextField) {
+        didEndEditing?(textField)
+    }
+    
+    public func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        shouldBeginEditing?(textField) ?? true
+    }
+    
+    public func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        shouldEndEditing?(textField) ?? true
+    }
+    
+    public func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        shouldClear?(textField) ?? true
+    }
+    
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        shouldReturn?(textField) ?? true
     }
 }
 
